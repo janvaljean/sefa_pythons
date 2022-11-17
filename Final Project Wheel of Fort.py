a5fad8c514a909fa8ae39cb170b1bd6ddaee5bfd
Janvaljean <sfkrtld@gmail.com>
2022-11-17 18:07:33 +0100

    mainclass WOFPlayer

---
+++ Final Project Wheel of Fort.py
@@ -0,0 +1,18 @@
+class WOFPlayer:
+    prizeMoney = 0
+    prizes = []
+    def __init__(self, name) -> None:
+        self.name = name
+    
+    def addMoney(self, amt):
+        return self.prizeMoney + amt
+    
+    def goBankrupt(self):
+        self.prizeMoney = 0
+        return self.prizeMoney
+    
+    def addPrize(self, prize):
+        return self.prizes.append(prize)    
+    
+    def __str__(self) -> str:
+        return f"{self.name} {(self.prizeMoney)}"
