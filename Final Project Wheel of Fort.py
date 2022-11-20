c330d44ccd1476218a676f2ed8505d17430df5bc
Janvaljean <sfkrtld@gmail.com>
2022-11-20 12:24:17 +0100

    update 4

--- Final Project Wheel of Fort.py
+++ Final Project Wheel of Fort.py
@@ -1,10 +1,14 @@
+import random
+VOWEL_COST = 250
+LETTERS = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+VOWELS = 'AEIOU'
 # #! PART A
 
-class WOFPlayer:
-    prizeMoney = 0
-    prizes = []
+class WOFPlayer():
     def __init__(self, name) -> None:
         self.name = name
+        self.prizeMoney = 0
+        self.prizes = []
     
     def addMoney(self, amt):
         return self.prizeMoney + amt
@@ -17,52 +21,49 @@ class WOFPlayer:
         return self.prizes.append(prize)    
     
     def __str__(self) -> str:
-        return f"{self.name} {(self.prizeMoney)}"
+        return f"{self.name} ${self.prizeMoney}"
     
 # #! PART B
 
 
 class WOFHumanPlayer(WOFPlayer):
-    
-    def getMove(self, category, obscuredPhrase, guessed):
-        print(f"{self.name} has ${self.prizeMoney}\n\nCategory: {category}\nPhrase: {obscuredPhrase}\nGuessed: {guessed}\n\nGuess a letter,phrase or type 'exit' or 'pass':")
-        guessed = input().upper
-        if guessed == 'exit':
-            return "exit the game"
-        elif guessed == 'pass':
-            return "skip your turn"
+
+    def getMove(category, obscuredPhrase, guessed):
+        str = input(self.name + "has $" + str(self.prizeMoney) + "/n" + ", Category:" + category + "/n" + ", Phrases:" +
+                    "/n" + obscuredPhrase + "/n" + ", Guessed:" + guessed + "/n" + "Guess a letter, phrase, or type 'exit' or 'pass':")
+        print(str)
         
 # ! PART C
 
 
 class WOFComputerPlayer(WOFPlayer):
-    import random
     SORTED_FREQUENCIES = 'ZQXJKVBPYGFWMUCLDRHSNIOATE'
-    super().__init__()
-    self.difficulty = 2
+    prizemoney = 0
+    def __init__(self):
+        super().__init__()
+        self.difficulty = difficulty
 
     def smartCoinFlip(self):
-        if self.difficulty < random.randint(1, 10):
-            return False
-        else:
+        if random.randint(1, 10) > self.difficulty:
             return True
+        else:
+            return False
 
     def getPossibleLetters(self, guessed):
-        LETTERS = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        LETTERS.pop(guessed)
-        VOWELS = list('AEIOU')
-        VOWEL_COST = 250
+        lst = list(LETTERS)
+        for c in guessed:
+            lst.remove(c)       
         if self.prizeMoney < VOWEL_COST:
-            for v in VOWELS:
-                LETTERS.pop(v)
-        return LETTERS
+            for l in lst:
+                lst.remove(l)
+        return lst
 
     def getMove(self, category, obscuredPhrase, guessed):
-        getPossibleLetters(guessed)
-        if LETTERS == VOWELS and self.prizeMoney < VOWEL_COST:
+        lst  
+        if getPossibleLetters(self, guessed) == VOWELS and self.prizeMoney < VOWEL_COST:
             return 'pass'
 
         elif smartCoinFlip(self) == True:
             return SORTED_FREQUENCIES[-1]
         elif smartCoinFlip(self) == False:
-            random.choice(getPossibleLetters())
+            return random.choice(getPossibleLetters())
